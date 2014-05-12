ec4ba9f1c88722fb10eef3a0d8e9865a1435e3a2
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-12 11:34:08 -0500

    cleaned up syntax / (mt_rand) for "rand"

--- highlow.php
+++ highlow.php
@@ -5,7 +5,7 @@ define('LOWEND', 1);
 define('TOPEND', 100);
 
 //  game picks a random number between 1 and 100.
-$comp_pick = rand(LOWEND, TOPEND);
+$comp_pick = mt_rand(LOWEND, TOPEND);
 
 
 
@@ -14,22 +14,32 @@ $comp_pick = rand(LOWEND, TOPEND);
 do {
 	fwrite (STDOUT, 'Pick a number between ' . LOWEND . ' and ' . TOPEND . '. ');
 
-$player_guess = fgets(STDIN);
-
-echo $player_guess;
-
-	if ($player_guess < $comp_pick) {
-		fwrite(STDOUT, "HIGHER!!! \n");
-	} elseif ($player_guess > $comp_pick) {
-		fwrite(STDOUT, "LOWER!!! \n");
-	} elseif ($player_guess == $comp_pick) {
-		fwrite(STDOUT, "WINNER!!! \n");
-} }   while ($player_guess != $comp_pick);
-
-// - if user's guess is more than the number, STDOUT "LOWER"
+	$player_guess = fgets(STDIN);
 
 
-// - if a user guesses the number, STDOUT "GOOD GUESS!"
+	echo $player_guess;
+
+		if ($player_guess < $comp_pick) {
+			// - if user's guess is less than the number, STDOUT "HIGHER"
+			fwrite(STDOUT, "HIGHER!!! \n");
+		} 
+		elseif ($player_guess > $comp_pick) {
+			// - if user's guess is more than the number, STDOUT "LOWER"
+			fwrite(STDOUT, "LOWER!!! \n");
+		} 
+		elseif ($player_guess == $comp_pick) {
+			// - if a user guesses the number, STDOUT "GOOD GUESS!"
+			fwrite(STDOUT, "WINNER!!! \n");
+			exit(0);
+		} 
+	}   while ($player_guess != $comp_pick);
+
+
+
+
+
+
+
 
 
 
