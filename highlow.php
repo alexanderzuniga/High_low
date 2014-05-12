be05df3040ba86c7c7c1e9f9d6da20a524fb84dd
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-12 16:22:00 -0500

    Fixed Order of operations for argc at top. Typos Added Notes

--- highlow.php
+++ highlow.php
@@ -1,14 +1,16 @@
 <?php
-
-if(!is_numeric($argv[1]) || !is_numeric($argv[2])) {
-	echo "I have no idea what you're talking about....Numbers please!\n";
-	exit(1);
-}
+//Limit to enter file name and two numbers. 
 if($argc != 3){
-	echo "Must enter \"php highlow.php\" and two differet numbers to begin.\n";
+	echo "Must enter \"php highlow.php\" and two different numbers to begin.\n";
 	exit(1);
 } 
+// Limit entries to being only numbers. 
+if (!is_numeric($argv[1]) || !is_numeric($argv[2])) {
+	echo "I have no idea what you\'re talking about! Enter two NUMBERS!\n"; 
+	exit(1);
+}
 
+// Allowing greater or lesser number to be entered in any order
 if ($argv[1] < $argv[2]) { 
 	define('LOWEND', (int)$argv[1]);
 	define('TOPEND', (int)$argv[2]);
@@ -19,7 +21,6 @@ if ($argv[1] < $argv[2]) {
 	echo "Must enter two different numbers to begin.\n";
 	exit(1);
 }
-
 
 //  game picks a random number between 1 and 100.
 $comp_pick = mt_rand(LOWEND, TOPEND);
@@ -41,7 +42,7 @@ do {
 	$counts++;
 
 	if ($player_guess < $comp_pick) {
-		fwrite(STDOUT, "HIGHER!!! Guess Agian. \n");
+		fwrite(STDOUT, "HIGHER!!! Guess Again. \n");
 	} elseif ($player_guess > $comp_pick) {
 		fwrite(STDOUT, "LOWER!!! Guess Again. \n");
 	
