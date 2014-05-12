9ffab601b6b709ec98575c288d191b3a0cbdc8cc
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-12 14:00:33 -0500

    Structural adjust and adding trim ( ) to name

--- highlow.php
+++ highlow.php
@@ -1,40 +1,36 @@
 <?php
-
-
-define('LOWEND', 1);
+ 
+define('LOWEND', 0);
 define('TOPEND', 100);
 
 //  game picks a random number between 1 and 100.
 $comp_pick = mt_rand(LOWEND, TOPEND);
+// Number of counts
 $counts = 0;
+//Initial Message
+fwrite (STDOUT, 'What is your name? ');
 
+$name = trim (fgets(STDIN));
 
+fwrite (STDOUT, 'Greetings ' . $name . '! '. PHP_EOL);
 
-	
+fwrite (STDOUT, 'Pick a number between ' . LOWEND .  ' and ' .  TOPEND . '! ');
+
+//Run Program
 do {
-	fwrite (STDOUT, 'Pick a number between ' . LOWEND . ' and ' . TOPEND . '. ');
 
 	$player_guess = fgets(STDIN);
-		$counts++;
+	$counts++;
 
-	echo $player_guess;
-
-		if ($player_guess < $comp_pick) {
-			// - if user's guess is less than the number, STDOUT "HIGHER"
-			fwrite(STDOUT, "HIGHER!!! \n");
-		} 
-		elseif ($player_guess > $comp_pick) {
-			// - if user's guess is more than the number, STDOUT "LOWER"
-			fwrite(STDOUT, "LOWER!!! \n");
-		} 
-		elseif ($player_guess == $comp_pick) {
-			// - if a user guesses the number, STDOUT "GOOD GUESS!"
-			fwrite(STDOUT, "WINNER!!! It took you $counts guesses to get it right! \n");
-			exit(0);
-		} 
-	}   while ($player_guess != $comp_pick);
-
-
-
+	if ($player_guess < $comp_pick) {
+		fwrite(STDOUT, "HIGHER!!! Guess Agian. \n");
+	} elseif ($player_guess > $comp_pick) {
+		fwrite(STDOUT, "LOWER!!! Guess Again. \n");
+	
+	} elseif ($player_guess == $comp_pick) {
+		fwrite(STDOUT, "WINNER!!! It took you {$counts} guesses to get it right! \n");
+		exit(0);
+	} 
+}   while ($player_guess != $comp_pick);
 
 ?>
